b2a21c9ccf4c44b5ea7b6ddca52dbb1219c08ba5
邹琳 <774249737@qq.com>
2020-07-16 18:56:36 +0800

    支持多张图片

--- icon.py
+++ icon.py
@@ -24,19 +24,8 @@ def un_zip(file_name):
         zip_file.extract(names,file_name + "_files/")
         imgs.append(names)
     
-    for img_path in imgs:
-        print(img_path)
-        newpath = ""
-        if "@2x.png" in img_path:
-            # 重命名新路径
-            newpath = infile + "/2.0x/" + img_path.replace("@2x", "")
-        elif "@3x.png" in img_path:
-            # 重命名新路径
-            newpath = infile + "/3.0x/" + img_path.replace("@3x", "")
-        else:
-            newpath = infile + '/' + img_path
-        #移动icon到目标路径
-        shutil.move(file_name + "_files/" + img_path, newpath)
+    for imgName in imgs:
+        move(file_name+"_files/", imgName)
     # 删除目录
     os.rmdir(file_name + "_files/")
     #删除压缩包
@@ -45,8 +34,28 @@ def un_zip(file_name):
     ==========完成==========
     ''')
     zip_file.close()
+
+def move(path, imgName):
+    newpath = ""
+    if "@2x.png" in imgName:
+        # 重命名新路径
+        newpath = infile + "/2.0x/" + imgName.replace("@2x", "")
+    elif "@3x.png" in imgName:
+        # 重命名新路径
+        newpath = infile + "/3.0x/" + imgName.replace("@3x", "")
+    else:
+        newpath = infile + '/' + imgName
+    #移动icon到目标路径
+    shutil.move(path + imgName, newpath)
     
 while(1):
     outpath = raw_input('icon压缩包:')
-    # 删除右边空格
-    un_zip(outpath.rstrip()) 
+
+    if os.path.isdir(outpath):
+        for parent,dirnames,filenames in os.walk(outpath):
+            for val in filenames:
+                print(val)
+                move(parent+"/", val)
+    else:
+        # 删除右边空格
+        un_zip(outpath.rstrip()) 
