18396c19ee8689a2c72aff4210014994afe63a74
zoulin <zoulin@supermonkey.com.cn>
2022-05-12 12:50:38 +0800

    python 图片重命名, 中文转简拼

--- icon.py
+++ icon.py
@@ -5,13 +5,11 @@
 import os
 import zipfile
 import shutil
-import sys
-reload(sys)
-sys.setdefaultencoding("utf-8")
+from xpinyin import Pinyin
 
-infile = raw_input("输入flutter资源目录:")
+infile = input("输入flutter资源目录:")
 infile = infile.rstrip()
-
+p = Pinyin()
 
 def un_zip(file_name):
     """unzip zip file"""
@@ -39,26 +37,47 @@ def un_zip(file_name):
 
 def move(path, imgName):
     newpath = ""
+    newname = ""
     if "@2x.png" in imgName:
         # 重命名新路径
-        newpath = infile + "/2.0x/" + imgName.replace("@2x", "")
+        newpath = infile + "/2.0x/"
+        newname = imgName.replace("@2x", "")
     elif "@3x.png" in imgName:
         # 重命名新路径
-        newpath = infile + "/3.0x/" + imgName.replace("@3x", "")
+        newpath = infile + "/3.0x/"
+        newname = imgName.replace("@3x", "")
     else:
-        newpath = infile + '/' + imgName
+        newpath = infile + '/'
+        newname = imgName
     #移动icon到目标路径
-    shutil.move(path + imgName, newpath)
+    newname = p.get_initials(newname.replace("-","_"),"").lower()
+    respath = get_path_name(newpath, newname, 0)
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+    shutil.move(path + imgName, respath)
+
+def get_path_name(path, name, index):
+    print(path + name)
+    if not os.path.exists(path + name):
+        return path + name
+    else:
+        _temp = "%s.png" % index
+        index += 1
+        if _temp in name:
+            name = name.replace(_temp, "%s.png" % index)
+        else:    
+            name = name.replace(".png", "%s.png" % index)
+        return get_path_name(path, name, index)
     
 while(1):
-    outpath = raw_input('icon压缩包或者解压过后的文件夹:')
+    outpath = input('icon压缩包或者解压过后的文件夹:')
     outpath = outpath.rstrip()
     if os.path.isdir(outpath):
         for parent,dirnames,filenames in os.walk(outpath):
             for val in filenames:
                 print(val)
                 move(parent+"/", val)
-            os.rmdir(parent)
+            # os.rmdir(parent)
     else:
         # 删除右边空格
         un_zip(outpath) 
